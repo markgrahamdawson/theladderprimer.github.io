d44c846abbcb1b7d5b23bc3d1974d8abe31b35cc
markd-hiyield <mark@hiyield.co.uk>
2022-10-09 21:49:51 +0100

    added basic nuxt template

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <nav style="display:flex; gap:10px;">
+      <NuxtLink href="/">Home</NuxtLink>
+      <NuxtLink href="/library">Library</NuxtLink>
+      <NuxtLink href="/rift">Rift</NuxtLink>
+      <NuxtLink href="/webappbuild">WebAppBuild</NuxtLink>
+    </nav>
+      <NuxtPage />
   </div>
 </template>
