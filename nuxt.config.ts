c8e6c171fc1f29bb5831c47fdb187bfa19a4f13f
markgrahamdawson <markgrahamdawson@gmail.com>
2022-10-23 16:28:06 +0100

    rmove app base

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,9 +5,9 @@ export default defineNuxtConfig({
   router: {
     base: '/theladderprimer.github.io/'
   },
-  app: {
-    baseURL: '/theladderprimer.github.io/'
-  },
+  // app: {
+  //   baseURL: '/theladderprimer.github.io/'
+  // },
   build: {
     postcss: {
       postcssOptions: require("./postcss.config.js"),
