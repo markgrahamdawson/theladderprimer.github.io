44b1d61336bd9e7687d1d275e0df92fd3c2a8d9c
markgrahamdawson <markgrahamdawson@gmail.com>
2022-10-25 09:23:57 +0100

    changed image layout

--- pages/webappdesign.vue
+++ pages/webappdesign.vue
@@ -6,6 +6,9 @@
         <h1 class="m-2 font-medium text-4xl mt-0 mb-8 text-black">Web App Design</h1>
         <div>
           <div class="m-2 flex">
+            <div><img src="~/assets/webexamples.png" alt="The Ladder Logo" class="object-cover w64 p-4"></div>
+          </div>
+          <div class="m-2 flex">
             <div><img src="~/assets/buildguide1.png" alt="The Ladder Logo" class="object-cover h-48 w-48 p-4"></div>
             <div><img src="~/assets/buildguide2.png" alt="The Ladder Logo" class="object-cover h-48 w-48 p-4"></div>
             <div><img src="~/assets/buildguide3.png" alt="Poster" class="object-cover w-48 p-4"></div>
@@ -18,7 +21,9 @@
             <div><img src="~/assets/poster.png" alt="Poster" class="object-cover w-48 p-4"></div>
             <div><img src="~/assets/design1.jpeg" alt="Example design" class="object-cover w-48 p-4"></div>
           </div>
-            The full brand guidelines are <a href="/static/The_ladder_brand_guidelines_01.pdf">here</a>
+
+          
+            The full brand guidelines are <a href="https://github.com/markgrahamdawson/theladderprimer.github.io/blob/gh-pages-bd0a239/static/The_ladder_brand_guidelines_01.pdf" class="text-teal">here</a>
           </div>
           <div>
               <embed src="/static/The_ladder_brand_guidelines_01.pdf" width="1200px" height="900" />
